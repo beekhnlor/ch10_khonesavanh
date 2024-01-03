d0b966521682a148443b5852ec435ac9be250e69
beekhnlor <beekingwordviengxay@gmail.com>
2024-01-03 23:13:32 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:lab10_khonesavanh_2cw3/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+
+
+     
+      home:  home(),
+    );
+  }
+}
+
+
